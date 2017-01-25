674aa8a6b5e187f149b1c9cb81930ec25cb7c484
THIBAUD adrian <thibaud@informatique.univ-paris-diderot.fr>
2017-01-25 16:44:17 +0100

    valeur des axes en pourcentage (de -100 a 100 en float) plus ajout du controller playstation

--- src/Manette/controller.c
+++ src/Manette/controller.c
@@ -21,102 +21,145 @@ char is_connect() {
 	return 0;
 }
 
+
+float pourcent(int valeur ,float reference){
+  return ((float) valeur) * 100 / ((float)reference);
+}
+
+float diff_axes(int axe_down, int axe_up, int val_max){
+  int val_down = axe_down + val_max;
+  int val_up = axe_up + val_max;
+  int val_general = val_up - val_down;
+  return pourcent(val_general, val_max * 2);
+}
+
 void control(args_CONTROLER * argsControl) {
+  
+  DataController * manette = argsControl->manette;
+  char verbose = argsControl->verbose;
+  
+  manette->moteur_active = 0;
+  SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK); // on initialise les sous-programmes vidéo et joystick
 
-	DataController * manette = argsControl->manette;
-	char verbose = argsControl->verbose;
+  if(verbose){
+    for(int j=0; j<SDL_NumJoysticks(); j++){
+      printf("controller %d : %s\n", j, SDL_JoystickName(j));
+    }
+  }
+  
+  
+  inputt input; // on crée la structure
+  initialiserInput(&input, 0); // on l'initialise au joystick n°0
 
-	manette->moteur_active = 0;
-	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK); // on initialise les sous-programmes vidéo et joystick
+  int modele;
+  int val_max = 32768;
+  
+  int quitter = 0;
+  
+  float tmpM0,tmpM1,tmpM2,tmpM3;
+  while (!quitter) {
+    
+    usleep(Update_Frequence);
+    
+    updateEvent(&input); // on récupère les évènements
+    
+    if(input.boutons[4] && input.boutons[5] && input.boutons[6]
+	&& input.boutons[7] || input.boutons[10] && input.boutons[11]) {
+      
+      modele = !(input.boutons[10] && input.boutons[11]);
+      //modele = 1 pour xbox controller, 0 for ps4 controller
 
-	inputt input; // on crée la structure
-	initialiserInput(&input, 0); // on l'initialise au joystick n°0
+      
+      manette->moteur_active = (manette->moteur_active) ? 0 : 1;
+      /*TODO
+	<=> manette->moteur_active = !(manette->moteur_active)
+      */
+      input.boutons[4] = input.boutons[5] = input.boutons[6] =
+	input.boutons[7] = input.boutons[10] = input.boutons[11] = 0;
+      
+      if(verbose){printf("bool_moteur_active= %d\n", manette->moteur_active);}
 
-	int quitter = 0;
+      while(!modele && (input.axes[3]!=-val_max || input.axes[4]!=-val_max)){
+	updateEvent(&input);
+      }
+      sleep(3);
+      
+      pthread_mutex_lock(&argsControl->pmutexControlerPlug->mutex);
+      pthread_cond_signal(&argsControl->pmutexControlerPlug->condition);
+      pthread_mutex_unlock(&argsControl->pmutexControlerPlug->mutex);
+    }
+    
+    //printf("APRES IF\n");
+    if (manette->moteur_active) {
+      /*
+      tmpM0 = (input.axes[0] < 0) ?
+	(float) input.axes[0] * -1 * 5.0 / 32768 + 5 :
+	(float) input.axes[0] * 5.0 / 32768 + 5;
+      tmpM1 = (input.axes[1] < 0) ?
+	(float) input.axes[1] * -1 * 5.0 / 32768 + 5 :
+	(float) input.axes[1] * 5.0 / 32768 + 5;
+      tmpM2 = (input.axes[3] < 0) ?
+	(float) input.axes[3] * -1 * 5.0 / 32768 + 5 :
+	(float) input.axes[3] * 5.0 / 32768 + 5;
+      tmpM3 = (input.axes[4] < 0) ?
+	(float) input.axes[4] * -1 * 5.0 / 32768 + 5 :
+	(float) input.axes[4] * 5.0 / 32768 + 5;
+      */
+      
 
-	float tmpM0,tmpM1,tmpM2,tmpM3;
-
-	while (!quitter) {
-
-		usleep(Update_Frequence);
-
-		updateEvent(&input); // on récupère les évènements
-
-		if (input.boutons[4] && input.boutons[5] && input.boutons[6]
-				&& input.boutons[7]) {
-			manette->moteur_active = (manette->moteur_active) ? 0 : 1;
-			/*TODO
-			  <=> manette->moteur_active = !(manette->moteur_active)
-			*/
-			input.boutons[4] = input.boutons[5] = input.boutons[6] =
-					input.boutons[7] = 0;
-			if(verbose){printf("bool_moteur_active= %d\n", manette->moteur_active);}
-			sleep(3);
-
-			pthread_mutex_lock(&argsControl->pmutexControlerPlug->mutex);
-			pthread_cond_signal(&argsControl->pmutexControlerPlug->condition);
-			pthread_mutex_unlock(&argsControl->pmutexControlerPlug->mutex);
-		}
-
-		//printf("APRES IF\n");
-		if (manette->moteur_active) {
-		  
-			tmpM0 = (input.axes[0] < 0) ?
-					(float) input.axes[0] * -1 * 5.0 / 32768 + 5 :
-					(float) input.axes[0] * 5.0 / 32768 + 5;
-			tmpM1 = (input.axes[1] < 0) ?
-					(float) input.axes[1] * -1 * 5.0 / 32768 + 5 :
-					(float) input.axes[1] * 5.0 / 32768 + 5;
-			tmpM2 = (input.axes[3] < 0) ?
-					(float) input.axes[3] * -1 * 5.0 / 32768 + 5 :
-					(float) input.axes[3] * 5.0 / 32768 + 5;
-			tmpM3 = (input.axes[4] < 0) ?
-					(float) input.axes[4] * -1 * 5.0 / 32768 + 5 :
-					(float) input.axes[4] * 5.0 / 32768 + 5;
-		  
-
-/*
-		  tmpM0 = input.axes[0];
-
-		  tmpM1 = input.axes[1];
-
-		  tmpM2 = input.axes[3];
-
-		  tmpM3 = input.axes[4];
-*/
-		  
-			pthread_mutex_lock(&argsControl->pmutexReadDataController->mutex);
-
-			argsControl->newThing = 1;
-			manette->axe_Rotation =tmpM0;
-			manette->axe_UpDown =tmpM1;
-			manette->axe_LeftRight =tmpM2;
-			manette->axe_FrontBack =tmpM3;
-
-			pthread_mutex_unlock(&argsControl->pmutexReadDataController->mutex);
-
-			if(verbose){printf("Axes 1: %f,%f     -       Axes 2: %f,%f \n ",
-					manette->axe_Rotation, manette->axe_UpDown, manette->axe_LeftRight,
-					manette->axe_FrontBack);}
-
-			  sleep(3);
-			}
-
-
-			if (!is_connect()) {
-				manette->moteur_active = 0;
-				if(verbose){printf("Plus de manette %d.\n", manette->moteur_active);}
-				break;
-			}
-
-		}
-
-
-
-	/* diverses destruction ...*/
-
-	detruireInput(&input); // on détruit la structure input
-	SDL_Quit(); // on quitte la SDL
-
-	return;
+      if(verbose){
+	printf("axe_Up = %d\taxe_Down = %d\n", input.axes[4], input.axes[3]);
+      }
+      
+      tmpM0 = modele ?
+	pourcent(input.axes[0], val_max) :
+	pourcent(input.axes[2], val_max);
+	  
+      tmpM1 = modele ?
+	pourcent(-1 * input.axes[1], val_max) :
+	(diff_axes(input.axes[3], input.axes[4], val_max));
+	  
+      tmpM2 = modele ?
+	pourcent(input.axes[3], val_max) :
+	pourcent(input.axes[0], val_max);
+	
+      tmpM3 = modele ?
+	pourcent(-1 * input.axes[4], val_max) :
+	pourcent(-1 * input.axes[1], val_max);
+	
+      
+      pthread_mutex_lock(&argsControl->pmutexReadDataController->mutex);
+      
+      argsControl->newThing = 1;
+      manette->axe_Rotation =tmpM0;
+      manette->axe_UpDown =tmpM1;
+      manette->axe_LeftRight =tmpM2;
+      manette->axe_FrontBack =tmpM3;
+      
+      pthread_mutex_unlock(&argsControl->pmutexReadDataController->mutex);
+      
+      if(verbose){printf("Axe_rotation: %f, axe_Up_Down: %f     -       Axes_LeftRight: %f, axe_FrontBack: %f \n ",
+			 manette->axe_Rotation, manette->axe_UpDown, manette->axe_LeftRight,
+			 manette->axe_FrontBack);}
+      
+      sleep(1);
+    }
+    
+    
+    if (!is_connect()) {
+      manette->moteur_active = 0;
+      if(verbose){printf("Plus de manette %d.\n", manette->moteur_active);}
+      break;
+    }
+    
+  }
+  
+  
+  
+  /* diverses destruction ...*/
+  
+  detruireInput(&input); // on détruit la structure input
+  SDL_Quit(); // on quitte la SDL
+  
+  return;
 }
