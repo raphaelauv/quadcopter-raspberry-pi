1ba778b33d1567e468ef045d2585f4931f0331bd
raphaelauv <raphaelauv@users.noreply.github.com>
2017-01-27 11:34:38 +0100

    Petit Automatisation code Calibration

---
+++ src/Calibrage/Calibration_ESC.h
@@ -0,0 +1,18 @@
+//
+//  Calibration_ESC.h
+//  Drone
+//
+//  Created by andres quiroz on 25/01/2017.
+//  Copyright © 2017 andres quiroz. All rights reserved.
+//
+
+#ifndef Calibration_ESC_h
+#define Calibration_ESC_h
+
+#include "../concurrent.h"
+#include "../motors.h"
+#include "../controldeVol.h"
+
+void * calibrate(void * args);
+
+#endif /* Calibration_ESC_h */
